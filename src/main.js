c108c16acfd5ae6ffba6faa7f41c90cc411797ac
Mariusz Poliński <mariusz.polinski@instapage.com>
2020-03-11 21:57:56 +0100

    Firebase included

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import VueSession from 'vue-session'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from './router'
+import Vue from "vue";
+import Vuex from "vuex";
+import VueSession from "vue-session";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import firebase from "firebase";
+import "firebase/firestore";
+import firebaseConfig from "../firebase.config";
 
-Vue.config.productionTip = false
-Vue.use(Vuex)
-Vue.use(VueSession)
+firebase.initializeApp(firebaseConfig);
+// firebase utils
+const db = firebase.firestore();
+console.log(db.collection("users"));
+// const auth = firebase.auth()
+// const currentUser = auth.currentUser
+
+Vue.config.productionTip = false;
+Vue.use(Vuex);
+Vue.use(VueSession);
 
 new Vue({
   vuetify,
   router,
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
